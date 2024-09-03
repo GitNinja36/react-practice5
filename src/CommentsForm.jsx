653443c9a5b6449a78d01e5429620897741807f5
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-09-03 19:29:26 +0530

    try using formic for validation for forms

--- src/CommentsForm.jsx
+++ src/CommentsForm.jsx
@@ -1,32 +1,55 @@
 import { useState } from "react"
+import { useFormik } from 'formik';
+
+const validate = values => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = 'Required';
+    } else if (values.username.length > 15) {
+      errors.username = 'Must be 15 characters or less';
+    }
+    return errors;
+  };
 
 export default function CommentsForm({addNewComment}){
-    let [formData, setFormData] = useState({
-        username: "",
-        remarks: "",
-        rating: 5,
+    // let [formData, setFormData] = useState({
+    //     username: "",
+    //     remarks: "",
+    //     rating: 5,
+    // });
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            remarks: '',
+            rating: 5,
+        },
+        validate,
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
     });
 
-    function handelInputChnage(event){
-        setFormData((currData)=>{
-            return{...currData, [event.target.name] : event.target.value};
-        })
-    }
+    // function handelInputChnage(event){
+    //     setFormData((currData)=>{
+    //         return{...currData, [event.target.name] : event.target.value};
+    //     })
+    // }
 
-    function handelSubmit(event){
-        event.preventDefault();
-        addNewComment(formData);
+    // function handelSubmit(event){
+    //     event.preventDefault();
+    //     addNewComment(formData);
 
-        setFormData({
-            username: "",
-            remarks: "",
-            rating: 5        
-        })
-    }
+    //     setFormData({
+    //         username: "",
+    //         remarks: "",
+    //         rating: 5        
+    //     })
+    // }
+
     return(
         <div>
             <h2>give a comment</h2>
-            <form onSubmit={handelSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                <label htmlFor="username">Username</label>
                <br />
                 <input 
@@ -34,9 +57,12 @@ export default function CommentsForm({addNewComment}){
                     id="username" 
                     placeholder="username" 
                     type="text" 
-                    value={formData.username}
-                    onChange={handelInputChnage}
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
                 />
+                {formik.touched.username && formik.errors.username ? (
+                <div>{formik.errors.username}</div>
+                ) : null}
                 <br /><br/>
 
                 <label htmlFor="remarks">Remarks</label>
@@ -47,8 +73,8 @@ export default function CommentsForm({addNewComment}){
                     cols="40" 
                     rows="3" 
                     placeholder="add remarks" 
-                    value={formData.remarks}
-                    onChange={handelInputChnage}
+                    value={formik.values.remarks}
+                    onChange={formik.handleChange}
                 >remarks</textarea>
                 <br /><br />
 
@@ -60,8 +86,8 @@ export default function CommentsForm({addNewComment}){
                     type="number" 
                     min={1} 
                     max={5} 
-                    value={formData.rating} 
-                    onChange={handelInputChnage}
+                    value={formik.values.rating}
+                    onChange={formik.handleChange}
                 />
                 <br />
                 <button type="submit">Add commit</button>
